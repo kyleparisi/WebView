9cda6836b2228bc9c1757fa6db0f344c361b7a3f
Kyle Parisi <kyleparisi@gmail.com>
2018-12-12 08:39:00 -0500

    Init

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -7,21 +7,19 @@
 //
 
 import Cocoa
+import WebKit
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    let url = "https://buildapart.io"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let request = URLRequest(url: URL(string: url)!)
+        webView.load(request)
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
 
