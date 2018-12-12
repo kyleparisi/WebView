9bcb924402037ae407b6d0db08b231735330fda3
Kyle Parisi <kyleparisi@gmail.com>
2018-12-12 07:48:46 -0500

    Initial Commit

---
+++ WebView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WebView
+//
+//  Created by Kyle Parisi on 12/12/18.
+//  Copyright © 2018 Kyle Parisi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WebView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WebView
+//
+//  Created by Kyle Parisi on 12/12/18.
+//  Copyright © 2018 Kyle Parisi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
